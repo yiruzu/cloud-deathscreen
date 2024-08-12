e12c32cf5b823439afc85edfcd617c96b13943ae
yiruzu <94981325+yiruzu@users.noreply.github.com>
2024-08-12 19:28:21 +0200

    Refactored config.lua

--- config.lua
+++ config.lua
@@ -1,30 +1,30 @@
 return {
 	Framework = "esx", -- "esx" or "qbcore"
 
-	Voice = "pma-voice", -- "pma-voice" or "saltychat"
-	-- set to "Voice = false," if you don't want the voice chat deafen feature when dead
+	Voice = "pma-voice", -- "pma-voice" or "saltychat". Set to "Voice = false," to disable the voice chat deafen feature when dead
 
-	Timer = 600, -- time until death in seconds
+	MainTimer = 600, -- Time (in seconds) until the player is fully dead
+	FaceDeathTimer = 5, -- Time (in seconds) until the player can click the "FACE DEATH" button
 
-	PriceForDeath = 1000, -- the price the player has to pay for clicking "FACE DEATH"
+	PriceForDeath = 1000, -- The amount of money the player has to pay for clicking the "FACE DEATH" button
 
-	BlurUIBackground = true,
+	BlurUIBackground = true, -- Enables or disables blurring the UI background when the death screen is active
 
 	EnabledControls = {
-		245, -- [T] allows players to open the chat while dead
-		-- add more to your liking
+		245, -- Allows players to open the chat while dead ([T] by default)
+		-- Add additional control key codes if needed
 	},
 
 	DeathAnim = {
-		enabled = true, -- enables the death animation
+		enabled = true, -- Toggle to enable or disable the death animation
 		animDict = "combat@damage@writhe",
 		animName = "writhe_loop",
 	},
 
 	Locales = {
 		MoneyRemoved = "You paid ~b~$%s~s~ for your hospital bill!",
-		NoMoney = "You dont have enough Money!",
+		NoMoney = "You don't have enough Money!",
 	},
 
-	RespawnCoords = vec4(305.6880, -587.5449, 43.2676, 69.8893), --! only for esx bridge
+	RespawnCoords = vec4(305.6880, -587.5449, 43.2676, 69.8893), -- Respawn coordinates --! (ONLY FOR ESX)
 }
