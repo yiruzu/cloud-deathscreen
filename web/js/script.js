dba689d0e9a579dbcc6293f77e147b0938c54105
yiruzu <94981325+yiruzu@users.noreply.github.com>
2024-08-12 19:30:49 +0200

    Refactored script.js & Added "FaceDeathTimer"

--- web/js/script.js
+++ web/js/script.js
@@ -2,19 +2,23 @@ const audioElement = new Audio("/web/sound/heartbeat.mp3");
 audioElement.loop = true;
 audioElement.volume = 0.35; // Set audio volume
 
-let currentTimer = 0;
+let mainTimer = 0;
+let faceDeathTimer = 0;
 let isDead = false;
-let timerId;
+let faceDeathEnabled = false;
+let mainTimerId;
+let faceDeathTimerId;
 
 window.addEventListener("message", handleMessage);
 
 function handleMessage(event) {
-  const { action, status, timer } = event.data;
+  const { action, status, timers } = event.data;
 
   if (action === "show") {
     handleShow(status);
-  } else if (action === "setTimer") {
-    handleSetTimer(timer);
+  } else if (action === "setTimers") {
+    handleSetMainTimer(timers.mainTimer);
+    handleSetFaceDeathTimer(timers.faceDeathTimer);
   }
 }
 
@@ -26,32 +30,54 @@ function handleShow(status) {
   }
 }
 
-function handleSetTimer(timer) {
-  clearTimeout(timerId);
-  currentTimer = timer;
-  timerId = setInterval(timerTick, 1000);
+function handleSetMainTimer(timer) {
+  clearInterval(mainTimerId);
+  mainTimer = timer;
+  mainTimerId = setInterval(mainTimerTick, 1000);
 }
 
-function timerTick() {
+function handleSetFaceDeathTimer(timer) {
+  clearInterval(faceDeathTimerId);
+  faceDeathTimer = timer;
+  faceDeathTimerId = setInterval(faceDeathTimerTick, 1000);
+}
+
+function mainTimerTick() {
   if (!isDead) return;
 
-  if (currentTimer <= 0) {
-    onTimeExpired();
+  if (mainTimer <= 0) {
+    onMainTimerExpired();
   } else {
-    updateTimerDisplay(currentTimer);
-    currentTimer--;
+    updateMainTimerDisplay(mainTimer);
+    mainTimer--;
   }
 }
 
-function onTimeExpired() {
+function faceDeathTimerTick() {
+  if (!isDead) return;
+
+  if (faceDeathTimer <= 0) {
+    onFaceDeathTimerExpired();
+  } else {
+    faceDeathTimer--;
+  }
+}
+
+function onMainTimerExpired() {
   $("#wrapper").fadeOut(250);
   triggerCallback("time_expired");
-  clearTimeout(timerId);
+  clearInterval(mainTimerId);
   resetState();
 }
 
-function updateTimerDisplay(time) {
-  const minutes = Math.floor(time / 60).toString().padStart(2, "0");
+function onFaceDeathTimerExpired() {
+  faceDeathEnabled = true;
+  $("#face-death").removeClass("disabled");
+  clearInterval(faceDeathTimerId);
+}
+
+function updateMainTimerDisplay(time) {
+  const minutes = Math.floor(time / 60) .toString() .padStart(2, "0");
   const seconds = (time % 60).toString().padStart(2, "0");
 
   $("#timer span:eq(0)").text(minutes[0]);
@@ -62,22 +88,34 @@ function updateTimerDisplay(time) {
 
 function startDeathSequence() {
   isDead = true;
-  currentTimer = 0;
+  faceDeathEnabled = false;
+  mainTimer = 0;
+  faceDeathTimer = 0;
   $("#wrapper").fadeIn(250);
+  $("#face-death").addClass("disabled");
   $("#call-emergency").removeClass("disabled");
   audioElement.play();
 }
 
 function resetState() {
   isDead = false;
-  currentTimer = 0;
+  faceDeathEnabled = false;
+  mainTimer = 0;
+  faceDeathTimer = 0;
+  clearInterval(mainTimerId);
+  clearInterval(faceDeathTimerId);
   $("#wrapper").fadeOut(250);
   audioElement.pause();
   audioElement.currentTime = 0;
+  $("#face-death").addClass("disabled");
 }
 
 $(function () {
-  $("#face-death").click(() => triggerCallback("face_death"));
+  $("#face-death").click(() => {
+    if (faceDeathEnabled) {
+      triggerCallback("face_death");
+    }
+  });
   $("#call-emergency").click(() => {
     triggerCallback("send_distress_signal");
     $("#call-emergency").addClass("disabled");
