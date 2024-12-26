692b9f036c61407129f75a12f082159632d5616f
yiruzu <yiruzu@protonmail.com>
2024-12-26 21:26:59 +0100

    chore(fxmanifest): bump version from 2.0.0 to 2.5.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,17 +5,19 @@ lua54 "yes"
 use_experimental_fxv2_oal "yes"
 
 author "yiruzu"
-description "Cloud Service - Deathscreen"
-version "2.0.0"
+description "Cloud Resources - Deathscreen"
+version "2.5.0"
 
 discord "https://discord.gg/jAnEnyGBef"
+documentation "https://cloud-resources.gitbook.io/docs/free-resources/cloud-deathscreen"
+
 repository "https://github.com/yiruzu/cloud-deathscreen"
 license "CC BY-NC"
 
 file { "shared/sh_config.lua", "shared/sh_locales.lua" }
 shared_scripts { "@ox_lib/init.lua", "shared/sh_functions.lua" }
-server_scripts { "bridge/server/**.lua", "server.lua" }
-client_scripts { "bridge/client/**.lua", "client.lua" }
+server_scripts { "bridge/server/*.lua", "server/*.lua" }
+client_scripts { "bridge/client/*.lua", "client/*.lua" }
 
-ui_page { "web/index.html" }
-files { "web/**/*" }
+ui_page { "web/build/index.html" }
+files { "web/build/**/*" }
